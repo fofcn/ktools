3f7ac6e610e862a864a41c27cda6dcc396d92f89
ford_ji <ford_ji@epam.com>
2024-04-16 16:58:03 +0800

    test

--- app/pdf/views.py
+++ app/pdf/views.py
@@ -33,6 +33,25 @@ def __split_pdf(source_file_path, pages_per_file):
         # 确定分割的范围
         for page_num in range(page_start, min(page_start + pages_per_file, total_pages)):
             output_pdf.insert_pdf(source_pdf, from_page=page_num, to_page=page_num)
+
+        for each_page in output_pdf:
+            # image_list = each_page.getImageList()
+            #  for image_info in image_list:
+            #     pix = fitz.Pixmap(output_pdf, image_info[0])
+            #      png = pix.tobytes()  # return picture in png format
+            #     if png == watermark_image:
+            #        document._deleteObject(image_info[0])
+            xref = each_page.get_contents()[0]
+            cont = bytearray(each_page.read_contents())
+            if cont.find(b"/Subtype/Watermark") > 0:
+                current_app.logger.info("marked-content watermark present")
+            while True:
+                i1 = cont.find(b"/Artifact")  # start of definition
+                if i1 < 0: break  # none more left: done
+                i2 = cont.find(b"EMC", i1)  # end of definition
+                cont[i1-2 : i2+3] = b""  # remove the full definition source "q ... EMC"
+            output_pdf.update_stream(xref, cont)  # replace the original source
+        
         output_filename = f"{filename}_part_{part_number}{file_extension}"
         # 将输出文档保存为新文件
         output_pdf.save(output_filename, garbage=4, deflate=True)
